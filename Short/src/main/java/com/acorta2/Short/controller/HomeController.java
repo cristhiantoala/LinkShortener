fc098c054255d57b4a0f0398af432f8312e15c97
Cristhian Maldonado <crismaldonado96@hotmail.com>
2024-07-10 17:09:08 -0500

    Removed nested git repository

---
+++ Short/src/main/java/com/acorta2/Short/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.acorta2.Short.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Bienvenidos al Acortador de Links!";
+    }
+}
---
+++ Short/src/test/java/com/acorta2/Short/ShortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acorta2.Short;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
